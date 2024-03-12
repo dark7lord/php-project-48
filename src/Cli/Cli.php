669b617f67bc72b0039b5a5d9f91cd636ea79394
gopal <gopal@student.21-school.ru>
2024-03-12 13:05:01 +0300

    add reading files (readJsonFile)

--- src/Cli/Cli.php
+++ src/Cli/Cli.php
@@ -1,8 +1,23 @@
 <?php
 
-namespace Gendiff\Cli;
+namespace Differ\Cli;
 
 use Docopt;
+use function Differ\Differ\gendiff;
+
+function readJsonFile($filename)
+{
+    if (file_exists($filename)) {
+        $fileContent = file_get_contents($filename);
+        $tree = json_decode($fileContent, true);
+        if ($tree === null && json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException("Error decoding JSON in file $filename");
+        }
+        return $tree;
+    } else {
+        throw new \RuntimeException("Error reading file $filename");
+    }
+}
 
 function startCli()
 {
@@ -12,13 +27,18 @@ Generate diff.
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <filename1> <filename2>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
 DOC;
 
-    $arguments = Docopt::handle($doc);
+    $params = [
+        'argv' => $_SERVER['argv']
+    ];
+
+    $arguments = Docopt::handle($doc, $params);
 
     if ($arguments['--help']) {
         echo $doc;
@@ -29,4 +49,22 @@ DOC;
         echo '1.0';
         exit;
     }
+
+    $filename1 = $arguments['<filename1>'];
+    $filename2 = $arguments['<filename2>'];
+
+    try {
+        $tree1 = readJsonFile($filename1);
+        $tree2 = readJsonFile($filename2);
+        print_r($tree1);
+        print_r($tree2);
+    } catch (\RuntimeException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+
+
+//    gendiff($file1, $file2);
+//
+//    print_r($file1);
 }
---
+++ src/Differ/Differ.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Differ\Differ;
+
+function gendiff($tree1, $tree2)
+{
+//    print_r("$tree1 and $tree2 and gendiff worlink boring");
+    print_r('gendiff work!');
+}
